077a7f921b311862afbbbe0e764a7138e2dcad65
JasperWong <766453486@qq.com>
2016-05-06 13:06:13 +0800

    merge

--- app/src/main/java/com/jasperwong/smartbicycle/ble/BluetoothLeService.java
+++ app/src/main/java/com/jasperwong/smartbicycle/ble/BluetoothLeService.java
@@ -66,6 +66,8 @@ public class BluetoothLeService extends Service {
     public final static UUID UUID_HEART_RATE_MEASUREMENT =
             UUID.fromString(SampleGattAttributes.HEART_RATE_MEASUREMENT);
 
+
+
     // Implements callback methods for GATT events that the app cares about.  For example,
     // connection change and services discovered.
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {

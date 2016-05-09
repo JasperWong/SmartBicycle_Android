1462ab6104e8d0b8590a396ce31a359098263429
JasperWong <766453486@qq.com>
2016-05-09 20:16:11 +0800

    finish map with drawer and toolbar

--- app/src/main/java/com/jasperwong/smartbicycle/activity/GuideActivity.java
+++ app/src/main/java/com/jasperwong/smartbicycle/activity/GuideActivity.java
@@ -21,7 +21,7 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.model.LatLng;
 import com.jasperwong.smartbicycle.R;
 
-public class GuideActivity extends BaseActivity implements View.OnClickListener {
+public class GuideActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
 
     private static final String LTAG = GuideActivity.class.getSimpleName();
     private MapView mMapView=null;
@@ -32,77 +32,78 @@ public class GuideActivity extends BaseActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
-        setContentView(R.layout.guide);
+//        setContentView(R.layout.guide);
+        setContentView(R.layout.activity_guide);
         mMapView=(MapView)findViewById(R.id.bmapView);
-//        setContentView(R.layout.activity_guide);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_guide);
-//        setSupportActionBar(toolbar);
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-//        Intent intent = getIntent();
-//        if (intent.hasExtra("x") && intent.hasExtra("y")) {
-//            // 当用intent参数时，设置中心点为指定点
-//            Bundle b = intent.getExtras();
-//            LatLng p = new LatLng(b.getDouble("y"), b.getDouble("x"));
-//            mMapView = new MapView(this,
-//                    new BaiduMapOptions().mapStatus(new MapStatus.Builder()
-//                            .target(p).build()));
-//        } else {
-//            mMapView = new MapView(this, new BaiduMapOptions());
-//        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_guide);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        Intent intent = getIntent();
+        if (intent.hasExtra("x") && intent.hasExtra("y")) {
+            // 当用intent参数时，设置中心点为指定点
+            Bundle b = intent.getExtras();
+            LatLng p = new LatLng(b.getDouble("y"), b.getDouble("x"));
+            mMapView = new MapView(this,
+                    new BaiduMapOptions().mapStatus(new MapStatus.Builder()
+                            .target(p).build()));
+        } else {
+            mMapView = new MapView(this, new BaiduMapOptions());
+        }
 //        setContentView(mMapView);
-//        mBaiduMap = mMapView.getMap();
+        mBaiduMap = mMapView.getMap();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_guide) {
+            Intent guideIntent= new Intent(this,GuideActivity.class);
+            startActivity(guideIntent);
+            // Handle the camera action
+        } else if (id == R.id.nav_switch) {
+            Intent switchIntent=new Intent(this,SwitchActivity.class);
+            startActivity(switchIntent);
+        } else if (id == R.id.nav_setting) {
+            Intent settingIntent=new Intent(this,SettingActivity.class);
+            startActivity(settingIntent);
+        }
+//        else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 //    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_guide) {
-//            Intent guideIntent= new Intent(this,GuideActivity.class);
-//            startActivity(guideIntent);
-//            // Handle the camera action
-//        } else if (id == R.id.nav_switch) {
-//            Intent switchIntent=new Intent(this,SwitchActivity.class);
-//            startActivity(switchIntent);
-//        } else if (id == R.id.nav_setting) {
-//            Intent settingIntent=new Intent(this,SettingActivity.class);
-//            startActivity(settingIntent);
-//        }
-////        else if (id == R.id.nav_manage) {
-////
-////        } else if (id == R.id.nav_share) {
-////
-////        } else if (id == R.id.nav_send) {
-////
+//    public void onBackPressed() {
+////        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+////        if (drawer.isDrawerOpen(GravityCompat.START)) {
+////            drawer.closeDrawer(GravityCompat.START);
+////        } else {
+////            super.onBackPressed();
 ////        }
 //
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
 //    }
-
-    @Override
-    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-    }
     @Override
     public void onClick(View v) {
     }
@@ -119,6 +120,7 @@ public class GuideActivity extends BaseActivity implements View.OnClickListener 
 
     protected void onDestroy(){
         super.onDestroy();
+        mMapView.onDestroy();
     }
 
 

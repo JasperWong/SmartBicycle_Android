303685b382a0bb7ebde8cccfe324a720c2975605
JasperWong <766453486@qq.com>
2016-07-28 19:54:26 +0800

    fix the rx

--- app/src/main/java/com/jasperwong/smartbicycle/service/BLEService.java
+++ app/src/main/java/com/jasperwong/smartbicycle/service/BLEService.java
@@ -195,8 +195,8 @@ public class BLEService extends Service
         final byte[] data = characteristic.getValue();
         if (data != null && data.length > 0) {
             final StringBuilder stringBuilder = new StringBuilder(data.length);
-            for(byte byteChar : data)
-                stringBuilder.append(String.format("%02X ", byteChar));
+//            for(byte byteChar : data)
+//                stringBuilder.append(String.format("%02X ", byteChar));
             intent.putExtra(EXTRA_DATA, new String(data) + "\n" + stringBuilder.toString());
         }
         sendBroadcast(intent);
